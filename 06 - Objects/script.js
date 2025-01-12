a3d5d666538e7d9865aaf02386421916522b6d9a
Yasin Cakal <whoisyasin@gmail.com>
2025-01-11 17:20:05 -0800

    test

--- 06 - Objects/script.js
+++ 06 - Objects/script.js
@@ -1,1 +1,17 @@
 // Objects = key : value pairs in {}
+function smash(words) {
+  if (words.length === 0) {
+    return ''
+  }
+  if (words.length === 1) {
+    return words[0]
+  }
+  let sentence = words[0]
+  for (let i = 1; i < words.length; i++) {
+    sentence += ' ' + words[i]
+  }
+  return sentence
+}
+
+words = ['hello', 'world', 'this', 'is', 'great']
+console.log(smash(words))
