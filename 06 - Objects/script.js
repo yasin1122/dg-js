ea69d18d2ad2c6da43b0d599106b5bf1fabda50e
Yasin Cakal <whoisyasin@gmail.com>
2025-01-15 19:44:10 -0800

    obj

--- 06 - Objects/script.js
+++ 06 - Objects/script.js
@@ -3,7 +3,14 @@
 const student = {
   name: 'Joe',
   passing: true,
-  testScores: [82, 75, 98]
+  testScores: [82, 75, 98],
+  testAverage: function () {
+    let total = 0
+    for (const score of this.testScores) {
+      total += score
+    }
+    return total / this.testScores.length
+  }
 }
 
-console.log(student.testScores[1])
+console.log(student.testScores[1], student.testAverage())
