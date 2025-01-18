f3be0dee6582ee3add26163b9511785d1f3b57c1
Yasin Cakal <whoisyasin@gmail.com>
2025-01-17 18:51:14 -0800

    errors

--- 08 - Errors/script.js
+++ 08 - Errors/script.js
@@ -1,3 +1,25 @@
 // Handling Errors
 'use strict'
-console.log('Errors')
+
+const makeError = () => {
+  try {
+    // throw new customError('This is a custom error!')
+    throw new Error('This is a generic error!')
+    const msg = 'Hello'
+    msg = 'World'
+  } catch (err) {
+    console.table(err)
+    console.warn(err)
+    console.error(err.name, err.message, err.stack)
+  } finally {
+    console.log('Finally block always executes!')
+  }
+}
+
+makeError()
+
+function customError(message) {
+  this.message = message
+  this.name = 'customError'
+  this.stack = `${this.name}" ${this.message}`
+}
