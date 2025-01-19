8da16fd60b35bbf24666c5654bdc76a9d067345e
Yasin Cakal <whoisyasin@gmail.com>
2025-01-18 19:11:14 -0800

    DOM

--- 09 - DOM/script.js
+++ 09 - DOM/script.js
@@ -45,3 +45,31 @@ console.log(evenDivs[0].parentElement.firstChild)
 console.log(evenDivs[0].parentElement.firstElementChild)
 console.log(evenDivs[0].parentElement.nextElementSibling)
 console.log(evenDivs[0].parentElement.previousSibling)
+
+view1.style.display = 'none'
+view2.style.display = 'flex'
+
+view2.style.flexDirection = 'row'
+view2.style.flexWrap = 'wrap'
+view2.style.margin = '10px'
+
+while (view2.lastChild) {
+  view2.lastChild.remove()
+}
+
+const createDivs = (parent, iter) => {
+  const newDiv = document.createElement('div')
+  newDiv.textContent = iter
+  newDiv.style.backgroundColor = '#000'
+  newDiv.style.width = '100px'
+  newDiv.style.height = '100px'
+  newDiv.style.margin = '10px'
+  newDiv.style.display = 'flex'
+  newDiv.style.justifyContent = 'center'
+  newDiv.style.alignItems = 'center'
+  parent.append(newDiv)
+}
+
+for (let i = 1; i <= 12; i++) {
+  createDivs(view2, i)
+}
