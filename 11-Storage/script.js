da1f5055233f0971e194280e04dc223f2cc31fb3
Yasin Cakal <whoisyasin@gmail.com>
2025-01-19 07:56:29 -0800

    local

--- 11-Storage/script.js
+++ 11-Storage/script.js
@@ -1,1 +1,15 @@
-//
+// Web Storage API
+
+const myObject = {
+  name: 'Dave',
+  logName: function () {
+    console.log(this.name)
+  }
+}
+const myArray = ['eat', 'sleep', 'code']
+
+sessionStorage.setItem('mySessionStore', JSON.stringify(myArray))
+const mySessionData = JSON.parse(sessionStorage.getItem('mySessionStore'))
+console.log(typeof mySessionData, myArray)
+
+localStorage.setItem('mySessionStore', JSON.stringify(myArray))
