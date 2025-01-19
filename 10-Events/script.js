bf03a6a26dcee97c711e98650ff6e27131d1f933
Yasin Cakal <whoisyasin@gmail.com>
2025-01-18 20:35:41 -0800

    add event

--- 10-Events/script.js
+++ 10-Events/script.js
@@ -1,3 +1,31 @@
 // Event Listeners
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Hide all views
+  document.getElementById('view1').style.display = 'none'
+  document.getElementById('view3').style.display = 'none'
+
+  // Show only view 2
+  document.getElementById('view2').style.display = 'flex'
+})
+
 const view = document.querySelector('#view2')
-console.log(view)
+const div = view.querySelector('div')
+const h2 = div.querySelector('h2')
+
+// Syntax: addEventListener(event, function, useCapture)
+
+const doSomething = () => {
+  alert('doing something')
+}
+
+h2.addEventListener('click', doSomething, false)
+h2.removeEventListener('click', doSomething, false)
+
+h2.addEventListener('click', event => {
+  if (event.target.textContent != 'Clicked!') {
+    event.target.textContent = 'Clicked!'
+  } else {
+    event.target.textContent = 'My 2nd View'
+  }
+})
