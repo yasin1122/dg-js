21b2e73d8a6fa759593c3d0df920bbd1d0b5130d
Yasin Cakal <whoisyasin@gmail.com>
2025-01-18 21:52:01 -0800

    prevent default

--- 10-Events/script.js
+++ 10-Events/script.js
@@ -3,10 +3,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Hide all views
   document.getElementById('view1').style.display = 'none'
-  document.getElementById('view3').style.display = 'none'
+  document.getElementById('view2').style.display = 'none'
 
   // Show only view 2
-  document.getElementById('view2').style.display = 'flex'
+  document.getElementById('view3').style.display = 'flex'
 })
 
 // Syntax: addEventListener(event, function, useCapture)
@@ -33,26 +33,45 @@ document.addEventListener('readystatechange', event => {
   }
 })
 
+// const initApp = () => {
+//   const view = document.querySelector('#view2')
+//   const div = view.querySelector('div')
+//   const h2 = div.querySelector('h2')
+
+//   view.addEventListener('click', event => {
+//     // event.stopPropagation()
+//     view.classList.toggle('purple')
+//     view.classList.toggle('darkblue')
+//   })
+
+//   div.addEventListener('click', event => {
+//     div.classList.toggle('blue')
+//     div.classList.toggle('black')
+//   })
+
+//   h2.addEventListener('click', event => {
+//     const myText = event.target.textContent
+//     myText === 'My 2nd View'
+//       ? (event.target.textContent = 'Clicked')
+//       : (event.target.textContent = 'My 2nd View')
+//   })
+
+//   const nav = document.querySelector('nav')
+//   nav.addEventListener('mouseover', event => {
+//     event.target.classList.add('height100')
+//   })
+//   nav.addEventListener('mouseout', event => {
+//     event.target.classList.remove('height100')
+//   })
+// }
+
+// view3 comment out initApp to make this work
+
 const initApp = () => {
-  const view = document.querySelector('#view2')
-  const div = view.querySelector('div')
-  const h2 = div.querySelector('h2')
-
-  view.addEventListener('click', event => {
-    // event.stopPropagation()
-    view.classList.toggle('purple')
-    view.classList.toggle('darkblue')
-  })
-
-  div.addEventListener('click', event => {
-    div.classList.toggle('blue')
-    div.classList.toggle('black')
-  })
-
-  h2.addEventListener('click', event => {
-    const myText = event.target.textContent
-    myText === 'My 2nd View'
-      ? (event.target.textContent = 'Clicked')
-      : (event.target.textContent = 'My 2nd View')
+  const view3 = document.querySelector('#view3')
+  const myForm = view3.querySelector('#myForm')
+  myForm.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log('submit event')
   })
 }
