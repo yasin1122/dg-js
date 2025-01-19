7c34af3a1980ed2cc0c4cc216b10015c66984caf
Yasin Cakal <whoisyasin@gmail.com>
2025-01-19 08:36:12 -0800

    module

---
+++ 12-Modules/mods.js
@@ -0,0 +1,7 @@
+export default function defaultFunction() {
+  return 'This is a default export function.'
+}
+
+export const exportedFunction = () => {
+  return 'This is a regular exported function'
+}
--- 12-Modules/script.js
+++ 12-Modules/script.js
@@ -1,1 +1,15 @@
+// Modules
 
+// import * as Mods from './mods.js'
+// console.log(Mods.exportedFunction())
+
+import defaultFunction from './mods.js'
+import { exportedFunction as expoFunc } from './mods.js'
+
+console.log(defaultFunction())
+console.log(expoFunc())
+
+import User from './user.js'
+
+const dave = new User('email@email.com', 'Dave')
+console.log(dave, dave.greeting())
