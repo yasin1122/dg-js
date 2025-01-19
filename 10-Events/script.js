184fe17d94c7c95513d286c2b2b058e2dbfa6f8b
Yasin Cakal <whoisyasin@gmail.com>
2025-01-18 21:03:34 -0800

    ready state

--- 10-Events/script.js
+++ 10-Events/script.js
@@ -9,23 +9,50 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('view2').style.display = 'flex'
 })
 
-const view = document.querySelector('#view2')
-const div = view.querySelector('div')
-const h2 = div.querySelector('h2')
-
 // Syntax: addEventListener(event, function, useCapture)
 
-const doSomething = () => {
-  alert('doing something')
-}
+// const doSomething = () => {
+//   alert('doing something')
+// }
 
-h2.addEventListener('click', doSomething, false)
-h2.removeEventListener('click', doSomething, false)
+// h2.addEventListener('click', doSomething, false)
+// h2.removeEventListener('click', doSomething, false)
 
-h2.addEventListener('click', event => {
-  if (event.target.textContent != 'Clicked!') {
-    event.target.textContent = 'Clicked!'
-  } else {
-    event.target.textContent = 'My 2nd View'
+// h2.addEventListener('click', event => {
+//   if (event.target.textContent != 'Clicked!') {
+//     event.target.textContent = 'Clicked!'
+//   } else {
+//     event.target.textContent = 'My 2nd View'
+//   }
+// })
+
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === 'complete') {
+    console.log('readyState: complete')
+    initApp()
   }
 })
+
+const initApp = () => {
+  const view = document.querySelector('#view2')
+  const div = view.querySelector('div')
+  const h2 = div.querySelector('h2')
+
+  view.addEventListener('click', event => {
+    // event.stopPropagation()
+    view.classList.toggle('purple')
+    view.classList.toggle('darkblue')
+  })
+
+  div.addEventListener('click', event => {
+    div.classList.toggle('blue')
+    div.classList.toggle('black')
+  })
+
+  h2.addEventListener('click', event => {
+    const myText = event.target.textContent
+    myText === 'My 2nd View'
+      ? (event.target.textContent = 'Clicked')
+      : (event.target.textContent = 'My 2nd View')
+  })
+}
