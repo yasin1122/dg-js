26b9f679f59783d9b2096b748c8d343761cef2d2
Yasin Cakal <whoisyasin@gmail.com>
2025-01-19 20:50:40 -0800

    async

--- 14-Async/script.js
+++ 14-Async/script.js
@@ -1,1 +1,57 @@
 // Callbacks, Promises, Thenables, and Async/Await
+
+// Callbacks leads to "callback hell"
+
+// Promises: Pending, Fulfilled, Rejected
+
+// const myPromise = new Promise((resolve, reject) => {
+//   const error = false
+//   if (!error) {
+//     resolve('Yes, resolved the promise!')
+//   } else {
+//     reject('No, rejected the promise.')
+//   }
+// })
+
+// console.log(myPromise)
+
+// myPromise
+//   .then(value => {
+//     return value + 1
+//   })
+//   .then(newValue => {
+//     console.log(newValue)
+//   })
+//   .catch(err => {
+//     console.error(err)
+//   })
+
+// const myNextPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('myNextPromise resolved!')
+//   }, 3000)
+// })
+
+// myNextPromise.then(value => {
+//   console.log(value)
+// })
+
+// console.log('This will print 3 seconds before.')
+
+// const users = fetch('https://jsonplaceholder.typicode.com/users')
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(data => {
+//     data.forEach(user => {
+//       console.log(user)
+//     })
+//   })
+
+// console.log(users) // this will log pending...
+
+const myUsers = {
+  userList: []
+}
