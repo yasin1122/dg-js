de0ed161a272731edd985868c182158a7272c983
Yasin Cakal <whoisyasin@gmail.com>
2025-01-28 19:35:04 -0800

    ll push

--- 00-Practice/script.js
+++ 00-Practice/script.js
@@ -1,4 +1,4 @@
-/////////////////////////////////////
+// Classes
 
 class Cookie {
   constructor(color) {
@@ -16,14 +16,47 @@ let cookieOne = new Cookie('green')
 cookieOne.setColor('yellow')
 console.log(cookieOne)
 
-//////////////////////////////////////
+// Pointers
 
 let obj1 = {
   value: 11
 }
 
 let obj2 = obj1
-obj1.value = 22
+obj1.value = 23
 console.log(obj2)
 
-//////////////////////////////////////
+// Linked Lists
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value)
+    this.head = newNode
+    this.tail = this.head
+    this.length = 1
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+let myLL = new LinkedList(11)
+myLL.push(22)
+console.log(myLL)
